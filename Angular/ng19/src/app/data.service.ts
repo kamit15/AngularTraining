07d997d8142747011c8212f7ce9a8284c6d345c0
Amit <amit.k15@outlook.com>
2025-02-14 13:36:04 +0530

    Add DataService with increment and decrement functionality and corresponding tests

---
+++ Angular/ng19/src/app/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  counter:number=0;
+
+  counterChange=new Subject<number>();
+ 
+  increment(){
+    this.counter++;
+    this.counterChange.next(this.counter);
+  }
+  decement(){
+    this.counter--;
+    this.counterChange.next(this.counter);
+  }
+}
